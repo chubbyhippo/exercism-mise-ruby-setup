82e2fcdbe4b4eabd57de345247dc339827287cc7
chubbyhippo <58356115+chubbyhippo@users.noreply.github.com>
2026-04-17 23:50:05 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,3 +2,4 @@
 curl -k "https://raw.githubusercontent.com/chubbyhippo/exercism-mise-ruby-setup/refs/heads/main/mise.toml" -o "mise.toml"
 mise trust
 mise install
+mise exec ruby -- gem install minitest
